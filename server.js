d3619bbdff21d4883028946b9ce93178de764858
Amanda Gettys <afgettys@gmail.com>
2020-07-18 20:03:58 -0400

    last update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 const app = express();
+const router = require("./routes/html-routes")
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -9,7 +10,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 require("./routes/routes.js")(app)
-require("./routes/html-routes.js")(app);
+
+app.use("/",router);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
