99f1bd9588185c1dd5a72b39ac9f85453a1d4da8
Amanda Gettys <afgettys@gmail.com>
2020-08-02 15:42:37 -0400

    adding files

--- routes/routes.js
+++ routes/routes.js
@@ -1,33 +1,31 @@
 var db = require("../models");
-const {DataTypes} = require("sequelize")
-const store = require("../models/store")(db.sequelize, DataTypes)
+const { DataTypes } = require("sequelize")
 
-module.exports = function(app) {
-    app.get("/api/store", function(req, res) {
-        
-        store.findAll({}).then(function(dbList) {
-        res.json(dbList);
+module.exports = function (app) {
+    app.get("/api/store", function (req, res) {
+
+        db.Store.findAll({}).then(function (dbList) {
+            res.json(dbList);
         });
-});
-    app.post("/api/store", function(req, res) {
-    console.log("Enter Here", req.body);
-        store.create({
-        name: req.body.itemName,
-        complete: req.body.complete
-        }).then(function(dbList) {
+    });
+    app.post("/api/item", function (req, res) {
+        console.log("Enter Here", req.body);
+        db.Item.create({
+            item: req.body.itemName
+        }).then(function (dbList) {
             res.json(dbList);
         })
-        .catch(function(err) {
-            res.json(err);
-});
+            .catch(function (err) {
+                res.json(err);
+            });
     });
-    app.delete("/api/store/:id", function(req, res) {
-        store.destroy({
-        where: {
-            id: req.params.id
-        }
-        }).then(function(dbList) {
-        res.json(dbList);
+    app.delete("/api/store/:id", function (req, res) {
+        db.Store.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbList) {
+            res.json(dbList);
         });
     });
 };
